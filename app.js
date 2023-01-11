7916fd59ecc9cc3082c9b8757685deb98ebae38e
Francis-Frimpong <brakay1136@gmail.com>
2023-01-11 19:39:16 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+//Accessing various html elements
+let hours = document.querySelector("#hours");
+let minutes = document.querySelector("#minutes");
+let seconds = document.querySelector("#seconds");
+
+const startBtn = document.querySelector(".start");
+const resetBtn = document.querySelector(".reset");
+const pauseBtn = document.querySelector(".purse");
+
+//Adding click events on the buttons
+startBtn.addEventListener("click", () => {
+  start();
+});
+
+resetBtn.addEventListener("click", () => {
+  reset();
+});
+
+pauseBtn.addEventListener("click", () => {
+  pause();
+});
+
+//Counter variables for hrs,min,sec
+let hrs = 0;
+let min = 0;
+let sec = 0;
+let setTime;
+
+//Button functionality
+
+//start function
+function start() {
+  setTime = setInterval(() => {
+    seconds.textContent = sec += 1;
+    if (sec === 60) {
+      seconds.textContent = sec = 0;
+      minutes.textContent = min += 1;
+    }
+    if (min === 60) {
+      hours.textContent = hrs += 1;
+      minutes.textContent = min = 0;
+      seconds.textContent = sec = 0;
+    }
+  }, 1000);
+}
+
+//pause function
+function pause() {
+  clearInterval(setTime);
+}
+
+//reset function
+function reset() {
+  clearInterval(setTime);
+  hours.textContent = hrs = 0;
+  minutes.textContent = min = 0;
+  seconds.textContent = sec = 0;
+}
